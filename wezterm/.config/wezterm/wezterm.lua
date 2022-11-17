64f0bb0625fbf9f921c9c9f863cff662e266580e
Sidhanth Holalkere <sidhanth.holalkere@gmail.com>
2022-11-17 10:07:54 -0500

    updated wezterm font

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require('wezterm')
 return {
     adjust_window_size_when_changing_font_size = false,
     color_scheme = 'Catppuccin Mocha',
+    font = wezterm.font('Jetbrains Mono'),
     font_size = 14.0,
     hide_tab_bar_if_only_one_tab = true,
     window_background_opacity = 0.9,
