4940100c9c4c31a67b189cbcbf837be055ab001c
Sidhanth Holalkere <sidhanth.holalkere@gmail.com>
2023-03-26 12:41:05 -0400

    update colorscheme

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,13 +1,3 @@
-local cat_ok, catppuccin = pcall(require, 'catppuccin')
-if not cat_ok then
-    return
-end
-
-local sol_ok, solarized = pcall(require, 'solarized')
-if not sol_ok then
-    return
-end
-
 local od_ok, onedark = pcall(require, 'onedark')
 if not od_ok then
     return
