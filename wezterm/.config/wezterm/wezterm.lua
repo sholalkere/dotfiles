f517129318495fa572d74ef30a70541427058588
Sidhanth Holalkere <sidhanth.holalkere@gmail.com>
2024-08-02 19:01:37 -0400

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,42 +1,62 @@
 local wezterm = require('wezterm')
 
+-- wezterm.on(
+--     'format-tab-title',
+--     function(tab, tabs, panes, config, hover, max_width)
+--         return string.format("%i", tab.tab_index)
+--     end
+-- )
+
+
 return {
     adjust_window_size_when_changing_font_size = false,
     audible_bell = 'Disabled',
     bold_brightens_ansi_colors = false,
     check_for_updates = false,
     colors = {
-        foreground = "rgb(94,97,101)",
-        background = "rgb(252,252,252)",
-        cursor_bg = "rgb(94,97,101)",
-        cursor_fg = "rgb(252,252,252)",
+        -- foreground = "rgb(94,97,101)",
+        -- background = "rgb(252,252,252)",
+	foreground = "#5c6166",
+	background = "#fcfcfc",
+        -- cursor_bg = "rgb(94,97,101)",
+        -- cursor_fg = "rgb(252,252,252)",
+	cursor_bg = "#5c6166",
+	cursor_fg = "#fcfcfc",
         cursor_border = "rgb(94,97,101)",
-        -- selection_fg,
-        -- selection_bg ,
-
-        ansi = {
-            "rgb(1,1,1)",
-            "rgb(196,117,109)",
-            "rgb(142,168,66)",
-            "rgb(211,170,95)",
-            "rgb(113,147,216)",
-            "rgb(143,117,189)",
-            "rgb(130,179,148)",
-            "rgb(193,193,193)",
-        },
-        brights = {
-            "rgb(52,52,52)",
-            "rgb(210,155,151)",
-            "rgb(176,207,84)",
-            "rgb(221,192,133)",
-            "rgb(142,171,224)",
-            "rgb(171,151,205)",
-            "rgb(153,194,170)",
-            "rgb(219,219,219)",
-        },
+        -- ansi = {
+        --     "rgb(1,1,1)",
+        --     "rgb(196,117,109)",
+        --     "rgb(142,168,66)",
+        --     "rgb(211,170,95)",
+        --     "rgb(113,147,216)",
+        --     "rgb(143,117,189)",
+        --     "rgb(130,179,148)",
+        --     "rgb(193,193,193)",
+        -- },
+	ansi = {
+	    '#010101',
+	    '#e7666a',
+	    '#80ab24',
+	    '#eba54d',
+	    '#4196df',
+	    '#9870c3',
+	    '#51b891',
+	    '#c1c1c1',
+	},
+	brights = {
+	    '#343434',
+	    '#ee9295',
+	    '#9fd32f',
+	    '#f0bc7b',
+	    '#6daee6',
+	    '#b294d2',
+	    '#75c7a8',
+	    '#dbdbdb',
+	},
         tab_bar = {
+            background = "rgb(219,219,219)",
             active_tab = {
-                bg_color = "rgb(193,193,193)",
+                bg_color = "rgb(252,252,252)",
                 fg_color = "rgb(1,1,1)",
             },
             inactive_tab = {
@@ -44,7 +64,7 @@ return {
                 fg_color = "rgb(52,52,52)",
             },
             inactive_tab_hover = {
-                bg_color = "rgb(210,210,210)",
+                bg_color = "rgb(230,230,230)",
                 fg_color = "rgb(52,52,52)",
             },
             new_tab = {
@@ -52,23 +72,25 @@ return {
                 fg_color = "rgb(52,52,52)",
             },
             new_tab_hover = {
-                bg_color = "rgb(210,210,210)",
+                bg_color = "rgb(230,230,230)",
                 fg_color = "rgb(52,52,52)",
             },
         },
     },
-    font = wezterm.font('Comic Code Ligatures'),
-    font_size = 16.0,
+    -- font = wezterm.font('RHComic Code Ligatures', { weight = 'DemiBold' }),
+    font = wezterm.font("SF Mono", { weight = "DemiBold" }),
+    -- font_size = 11,
+    front_end = 'WebGpu',
     hide_tab_bar_if_only_one_tab = true,
-    line_height = 1.0,
+    -- line_height = 1.25,
     show_update_window = false,
-    use_fancy_tab_bar = true,
-    window_background_opacity = 1.0,
-    window_decorations = "RESIZE",
+    -- window_background_opacity = 0.9,
+    -- macos_window_background_blur = 10,
+    -- window_decorations = "RESIZE",
     window_frame = {
-        font = wezterm.font('Comic Code Ligatures'),
+        font = wezterm.font('SF Mono'),
         active_titlebar_bg = "rgb(219,219,219)",
-        inactive_titlebar_bg = "rgb(219,219,219)",
+        inactive_titlebar_bg = "rgb(230,230,230)",
     }
 }
 
